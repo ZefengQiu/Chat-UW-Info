4743bc85f457a1b6f0bd1c01701374f8ab75d62e
ZefengQiu <z9qiu@uwaterloo.ca>
2015-11-21 20:41:36 -0500

    first commit

---
+++ Chat UW Info/Component/ChatInfoView/chatCells/SofaCell.swift
@@ -0,0 +1,24 @@
+//
+//  SofaCell.swift
+//  Chat UW Info
+//
+//  Created by Zefeng Qiu on 2015-11-21.
+//  Copyright © 2015 Qiu Zefeng. All rights reserved.
+//
+
+import UIKit
+
+class SofaCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
